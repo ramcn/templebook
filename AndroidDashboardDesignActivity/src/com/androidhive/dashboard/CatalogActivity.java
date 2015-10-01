40f6a09fc806a3caef39973de05e7e5ae002f815
ramcn <cn.ramachandra@gmail.com>
2015-10-01 19:19:55 +0530

    changes to connect to localhost and  display json data

--- AndroidDashboardDesignActivity/src/com/androidhive/dashboard/CatalogActivity.java
+++ AndroidDashboardDesignActivity/src/com/androidhive/dashboard/CatalogActivity.java
@@ -34,14 +34,80 @@ import androidhive.dashboard.R;
 public class CatalogActivity extends Activity {
 
  private List<Product> mProductList;
-  /** Called when the activity is first created. */
+ EditText etResponse;
+ TextView tvIsConnected;
+ 
+ public static String GET(String url){
+     InputStream inputStream = null;
+     String result = "";
+     try {
+         // create HttpClient
+         HttpClient httpclient = new DefaultHttpClient();
+         // make GET request to the given URL
+         HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+         // receive response as inputStream
+         inputStream = httpResponse.getEntity().getContent();
+         // convert inputstream to string
+         if(inputStream != null)
+             result = convertInputStreamToString(inputStream);
+         else
+             result = "Did not work!";
+     } catch (Exception e) {
+         Log.d("InputStream", e.getLocalizedMessage());
+     }
+     return result;
+ }
+ private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+
+     BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+     String line = "";
+     String result = "";
+     while((line = bufferedReader.readLine()) != null)
+         result += line;
+     inputStream.close();
+     return result;
+
+ }
+ public boolean isConnected(){
+     ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+         if (networkInfo != null && networkInfo.isConnected()) 
+             return true;
+         else
+             return false;   
+ }
+ private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+     @Override
+     protected String doInBackground(String... urls) {
+         return GET(urls[0]);
+     }
+     // onPostExecute displays the results of the AsyncTask.
+     @Override
+     protected void onPostExecute(String result) {
+         Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+         etResponse.setText(result);
+    }
+ }
+ /** Called when the activity is first created. */
  @Override
  public void onCreate(Bundle savedInstanceState) {
   super.onCreate(savedInstanceState);
   setContentView(R.layout.catalog);
 
+  // get reference to the views
+  etResponse = (EditText) findViewById(R.id.etResponse);
+  tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+  // check if you are connected or not
+  if(isConnected()){
+      tvIsConnected.setBackgroundColor(0xFF00CC00);
+      tvIsConnected.setText("You are conncted");
+  }
+  else{
+      tvIsConnected.setText("You are NOT conncted");
+  }
   // Obtain a reference to the product catalog
   mProductList = ShoppingCartHelper.getCatalog(getResources());
+  new HttpAsyncTask().execute("http://192.168.56.1:8080/pooja/temples.htm");
   
   // Create the list
   ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
